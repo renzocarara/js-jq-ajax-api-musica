b46bdf0079abb630c6c63aad44c2334d40ec6858
Renzo Carara <renzo.carara@libero.it>
2019-12-13 15:00:16 +0100

    struttura HTML-CSS

---
+++ main.js
@@ -0,0 +1,22 @@
+// Eseguire una chiamata ajax all'api di boolean: https://flynn.boolean.careers/exercises/api/array/music
+// che restituisce una lista di 10 dischi musicali.
+// Utilizzando handlebars, disegnare in pagina una card per ogni disco.
+// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette
+// all'utente di filtrare i dischi visualizzati in base al genere selezionato.
+// Nome repo: js-jq-ajax-api-musica
+// -----------------------------------------------------------------------------
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+    // https://flynn.boolean.careers/exercises/api/array/music
+
+}); // fine document ready
+
+// ---------------------------- FUNCTIONs --------------------------------------
